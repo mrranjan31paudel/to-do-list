c004818c2b4e72f38c81ffd177ae8baaefb565ee
mrranjan31paudel <paudeljimrranjan@gmail.com>
2020-01-08 16:49:47 +0545

    Updates

---
+++ precache-manifest.c65f5a0b083fc45f13efeb8a40a61cf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d4dc737bbfae8041fb422684a062f3a",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "dbbc643b4521a1e3c476",
+    "url": "/to-do-list/static/css/main.d59254a6.chunk.css"
+  },
+  {
+    "revision": "4fda46f54f0bff2442d5",
+    "url": "/to-do-list/static/js/2.eae7b3e8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/to-do-list/static/js/2.eae7b3e8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dbbc643b4521a1e3c476",
+    "url": "/to-do-list/static/js/main.66d360d6.chunk.js"
+  },
+  {
+    "revision": "41eaec7e616a8df64ff6",
+    "url": "/to-do-list/static/js/runtime-main.8ecd2aaf.js"
+  }
+]);
